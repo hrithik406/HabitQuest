f799d47a93f2f7b001c88c90a60c3897f5708246
Hrithik Raj <168572013+hrithik406@users.noreply.github.com>
2026-09-04 23:02:40 +0530

    cors modified

--- backend/server.ts
+++ backend/server.ts
@@ -9,7 +9,7 @@ import userRoutes from "./routes/userRoutes";
 
 const app: Application = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL ?? "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000", credentials: true }));
 app.use(express.json());
 
 app.use("/api/habits", habitRoutes);
